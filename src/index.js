0d69b5f984ea52c323bc72a77a5c3c4f2c9b4158
Evgeniy121 <36643137+Evgeniy121@users.noreply.github.com>
2018-02-20 17:59:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+var j=0;
+    for (var i=0 ; i<preferences.length; i++)
+{
+    if (i+1==preferences[ preferences[preferences[i]-1]-1]) {j++;}
+}
+    return(j/3);
+
+    };
